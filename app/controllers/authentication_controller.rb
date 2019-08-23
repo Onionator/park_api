01eadb269ff7f2dca22c909cacf8e2dfadecd666
Student <example@example.com>
2019-08-23 16:13:45 -0700

    define login in AuthenticationController.

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -6,7 +6,7 @@ class AuthenticationController < ApplicationController
     if @user&.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
-      render json: { token: token, epx: time.strfttime("%m-%d-%Y %H:%M"),
+      render json: { token: token, epx: time.strftime("%m-%d-%Y %H:%M"),
         username: @user.username }, status: :ok
       else
         render json: { error: 'Thou shalt authorize thine self forthwith.'}, status: :unauthorized
