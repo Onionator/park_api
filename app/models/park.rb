6ac3befb1ee668cbce4890181818441c2f79b917
Student <example@example.com>
2019-08-23 14:52:23 -0700

    add validations for the creation of a park.

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,2 +1,9 @@
 class Park < ApplicationRecord
+
+  validates :directions, presence: true
+  validates :description, presence: true
+  validates :name, presence: true
+  validates :states, presence: true
+  validates :website, presence: true
+
 end
