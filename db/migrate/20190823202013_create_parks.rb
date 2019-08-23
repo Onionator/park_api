e6645d2ac2ed79ddb920075a1946d03423b0d968
Student <example@example.com>
2019-08-23 13:22:53 -0700

    rebuild project and bring all files back to where they were before computer failure.

---
+++ db/migrate/20190823202013_create_parks.rb
@@ -0,0 +1,12 @@
+class CreateParks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :parks do |t|
+      create_table :parks do |t|
+        t.string :directions
+        t.text :description
+        t.string :name
+        t.string :states
+        t.string :website
+      end
+    end
+  end
